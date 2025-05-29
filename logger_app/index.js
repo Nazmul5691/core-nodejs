62ee2b499e437b8e2053eece0280acd68487c0db
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-29 22:02:13 +0600

    require path

--- logger_app/index.js
+++ logger_app/index.js
@@ -1,6 +1,20 @@
-console.log(process.argv);        // see path location of file
+const path = require('path')
+
+// console.log(process.argv);        // see path location of file
 
 
 const inputArguments = process.argv.slice(2)
 
-console.log(inputArguments);
+const text = inputArguments.join(' ')
+
+if(!text){
+    console.log('Please provide a message to log');
+    console.log('Example: node index.js hello world i am learning node js');
+    process.exit(1)
+}
+
+const filePath = path.join(__dirname, 'log.txt')
+
+
+
+console.log(filePath);
