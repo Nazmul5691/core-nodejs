95e7eafae57358739c4a45cfc18e88eaafe0f462
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-29 22:07:59 +0600

    require fs

--- logger_app/index.js
+++ logger_app/index.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 
 // console.log(process.argv);        // see path location of file
 
@@ -7,13 +8,19 @@ const inputArguments = process.argv.slice(2)
 
 const text = inputArguments.join(' ')
 
-if(!text){
+const message = `${text} \n`
+
+if(!message){
     console.log('Please provide a message to log');
     console.log('Example: node index.js hello world i am learning node js');
     process.exit(1)
 }
 
 const filePath = path.join(__dirname, 'log.txt')
+
+fs.appendFile(filePath, message, {encoding: 'utf8'}, () =>{
+    console.log('Your log added successfully');
+})
 
 
 
