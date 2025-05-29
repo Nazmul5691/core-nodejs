0b9fd9e6e55eb62b081fe25b932c80432565c204
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-30 01:23:12 +0600

    route create

--- ToDo_App/server.js
+++ ToDo_App/server.js
@@ -1,7 +1,19 @@
 const http = require("http")
 
+
 const server = http.createServer((req, res) =>{
-    res.end('Welcome to TODO app server')
+    // console.log(req.url, req.method);
+    // res.end('Welcome to TODO app server')
+
+    if(req.url === "/todos" && req.method === "GET"){
+        res.end("All todos");
+    }
+    else if (req.url === "/todos/create-todo" && req.method === "POST"){
+        res.end("todo created")
+    }
+    else{
+        res.end("route not found")
+    }
 })
 
 server.listen(5000, '127.0.0.1', () =>{
