9d4c2c26f356f48a2ed421d3c32235e67f0b6a57
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-31 17:57:54 +0600

    set response headers

--- ToDo_App/server.js
+++ ToDo_App/server.js
@@ -1,15 +1,46 @@
 const http = require("http")
 
+const data = [
+    {
+        "title" : "postgresql",
+        "body" : "learn postgresql"
+    },
+    {
+        "title" : "node",
+        "body" : "learn node"
+    },
+]
 
 const server = http.createServer((req, res) =>{
     // console.log(req.url, req.method);
     // res.end('Welcome to TODO app server')
 
     if(req.url === "/todos" && req.method === "GET"){
-        res.end("All todos");
+        // res.end("All todos");
+
+        // res.writeHead(201, {
+        //     "content-type" : "text/plain",
+        //     "email" : "new@email.com"
+        // })
+
+        res.writeHead(200, {
+            // "content-type" : "application/json",
+            "content-type" : "text/html",
+            // "email" : "new@email.com"
+        })
+        
+        // res.setHeader("content-type", "text/plain")
+        // res.setHeader("email", "new2@email.com")
+        // res.statusCode = 201
+
+        // res.end("Hello TODOS");
+        // res.end(JSON.stringify(data));
+        res.end(`<h1>hello world </h1> <h2>hello world </h2> <h3>hello world </h3>`);
     }
     else if (req.url === "/todos/create-todo" && req.method === "POST"){
-        res.end("todo created")
+        res.end({
+            "id" : 124
+        })
     }
     else{
         res.end("route not found")
